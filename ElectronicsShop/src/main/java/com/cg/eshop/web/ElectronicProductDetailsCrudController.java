6f632a2995cfc1d5eccfb21a965e8dc7264240ad
Wriju Banerjee <56797245+Rijubanerjee@users.noreply.github.com>
2021-05-22 16:53:21 +0530

    4.53pm

--- ElectronicsShop/src/main/java/com/cg/eshop/web/ElectronicProductDetailsCrudController.java
+++ ElectronicsShop/src/main/java/com/cg/eshop/web/ElectronicProductDetailsCrudController.java
@@ -5,12 +5,20 @@
  */
 package com.cg.eshop.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.eshop.dto.AddProductDto;
+import com.cg.eshop.dto.CategoryDto;
+import com.cg.eshop.dto.SuccessMessage;
 import com.cg.eshop.entity.ElectronicProductDetails;
+import com.cg.eshop.exception.CategoryNotFoundException;
 import com.cg.eshop.exception.ProductNotFoundException;
 import com.cg.eshop.service.IElectronicProductDetailService;
 
@@ -29,5 +37,12 @@ public class ElectronicProductDetailsCrudController {
 	public ElectronicProductDetails getProductDetailsByProductId(@PathVariable("prod_id") Integer productId)
 			throws ProductNotFoundException {
 		return electronicProductDetailService.getProductDetailsByProductId(productId);
+	}
+	@PostMapping("addproductdetails")
+	public SuccessMessage addProductDetails(@Valid @RequestBody AddProductDto addProductDto) throws CategoryNotFoundException {
+		ElectronicProductDetails elecproduct = electronicProductDetailService.addEletronicProduct(addProductDto);
+		SuccessMessage successMessage=new SuccessMessage("Your Generated Category ID "+ elecproduct.getProductID());
+		return successMessage;
+		
 	}
 }
